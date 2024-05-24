5acd659b03c589b77bdd2aa21f2e3445f3fe09af
yj <belle521834@gmail.com>
2024-05-25 02:13:15 +0900

    Todo Api 추가

---
+++ src/main/java/com/sparta/todo/controller/TodoController.java
@@ -0,0 +1,14 @@
+package com.sparta.todo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @PostMapping
+    public ResponseEntity postTodo(){
+        return ResponseEntity.ok().build();
+    }
+}
